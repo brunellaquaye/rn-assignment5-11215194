7b0eb2c198c3fe5d70b76aea8b6df07f7163fd4d
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-25 14:56:40 +0000

    feat: Implement tab navigation.

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+import SettingScreen from './SettingScreen';
+import MyCards from './MyCards';
+import Statistics from './Statistics';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Tab.Navigator >
+      <Tab.Screen name="Home" component={HomeScreen} options = {{headerShown:false}}/>
+  
+      <Tab.Screen name="My Cards" component={MyCards} options = {{headerShown:false}} />
+      <Tab.Screen name="Statistics" component={Statistics} options = {{headerShown:false}} />
+      <Tab.Screen name="Settings" component={SettingScreen} options = {{headerShown:false}} />
+    </Tab.Navigator>
+   </NavigationContainer>
   );
 }
 
@@ -18,3 +30,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
