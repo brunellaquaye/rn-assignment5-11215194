2e30c7c9f360b84ba5c45ee32be2fda69df77821
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-26 18:16:12 +0000

    feat: Add a themeContext file to specify styling and configurations for the dual themes.

---
+++ ThemeContext.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useContext } from "react";
+
+const lightTheme = {
+  dark: false,
+  colors: {
+    primary: "#6200ee",
+    background: "#ffffff",
+    text: "#000000",
+    logo: "#DFDFDF",
+    icon: "#000000",
+  },
+};
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    primary: "#bb86fc",
+    background: "#110426",
+    text: "#ffffff",
+    logo: "#1F1134",
+    icon: "#ffffff",
+  },
+};
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme.dark ? lightTheme : darkTheme));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+export const useTheme = () => useContext(ThemeContext);
