7a5dd84bc20bb6a09b10c292189ce7e7bc87d249
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-26 01:02:16 +0000

    docs: Add Statistics and Cards screen component.(file)

---
+++ MyCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const MyCards = () => {
+  return (
+    <View><Text>My cards</Text></View>
+  )
+}
+
+export default MyCards
---
+++ Statistics.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Statistics = () => {
+  return (
+    <View><Text>Statistics</Text></View>
+  )
+}
+
+export default Statistics
