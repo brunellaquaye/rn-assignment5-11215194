e0d36723214afb76ba4ac09ccc14642df8cdf18d
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-26 18:49:27 +0000

    feat: implement dark theme feature for the tab bar.

--- App.js
+++ App.js
@@ -4,66 +4,94 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from '@expo/vector-icons';
+import React, { useContext } from "react";
 
 import HomeScreen from "./HomeScreen";
 import SettingScreen from "./SettingScreen";
 import MyCards from "./MyCards";
 import Statistics from "./Statistics";
-import { ThemeProvider } from "./ThemeContext";
+import { ThemeProvider, ThemeContext } from "./ThemeContext";
 
-const greyColor = "#808080";
-const mainColor = "#2196F3";
+
 const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: theme.colors.background },
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.text,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.text}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="My Cards"
+        component={MyCards}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="creditcard"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.text}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={Statistics}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="pie-chart"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.text}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="setting"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.text}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <ThemeProvider>
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <AntDesign 
-              name="home" 
-              size={24} 
-              color={focused ? mainColor : greyColor} />
-            ),
-          }}
-        />
-
-        <Tab.Screen
-          name="My Cards"
-          component={MyCards}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-            
-              <AntDesign name="creditcard" size={24} color={focused ? mainColor : greyColor} />
-            ),
-           
-          }}
-        />
-        <Tab.Screen
-          name="Statistics"
-          component={Statistics}
-          options={{ headerShown: false,
-            tabBarIcon: ({ focused }) => (
-            
-            <Feather name="pie-chart" size={24} color={focused ? mainColor : greyColor} />
-          ), }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingScreen}
-          options={{ headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <AntDesign name="setting" size={24} color={focused? mainColor : greyColor} />
-          ) }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
