f85814074e6f54ae8e1e1dbc812cae4df269b300
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-26 03:15:12 +0000

    feat: Implement dark theme styling for the actions component

---
+++ components/DarkActions.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
+
+const Actions = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        marginTop: 30,
+        justifyContent: "space-around",
+      }}
+    >
+      <View>
+        <TouchableOpacity>
+          <View style={styles.icons}>
+            <AntDesign name="arrowup" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+
+        <Text style={styles.text}>Sent</Text>
+      </View>
+
+      <TouchableOpacity>
+        <View>
+          {/* Style the icon for the receive icon so that it looks aligned and centered */}
+          <View style={styles.icons}>
+            <AntDesign name="arrowdown" size={24} color="white" />
+          </View>
+          <Text style={styles.text}>Receive</Text>
+        </View>
+      </TouchableOpacity>
+      <View>
+      <TouchableOpacity>
+        <View style={styles.icons}>
+        <FontAwesome6 name="dollar" size={24} color="white" />
+        </View>
+        </TouchableOpacity>
+        <View>
+        <Text style={styles.text}>Loan</Text>
+        </View>
+      </View>
+
+      <View>
+      <TouchableOpacity>
+        <View style={styles.icons}>
+        <Ionicons name="cloud-upload-outline" size={25} color="white" />
+        </View>
+        </TouchableOpacity>
+        <View>
+        <Text style={styles.text}>Topup</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  icons: {
+    width: 50,
+    height: 50,
+    padding: 10,
+    backgroundColor: "#1F1134",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text:{
+    fontWeight: "light",
+    textAlign: "center",
+    color: "#939393",
+
+      }
+});
+
+export default Actions;
